f9817e95ed0cd895e3e7200dc3dd3b4b80c91bf3
Mishie Mangarateke <ST10371902@vcconnect.edu.za>
2025-07-27 19:20:24 +0200

    1st Commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace reniffects.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
