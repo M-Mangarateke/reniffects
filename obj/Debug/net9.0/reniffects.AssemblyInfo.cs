e4a12bcfd6188d30c8f028fe5fafa6b33d7cfba9
Mishie Mangarateke <mangamthe1@gmail.com>
2025-07-27 19:30:27 +0200

    1st commit

--- obj/Debug/net9.0/reniffects.AssemblyInfo.cs
+++ obj/Debug/net9.0/reniffects.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("reniffects")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f9817e95ed0cd895e3e7200dc3dd3b4b80c91bf3")]
 [assembly: System.Reflection.AssemblyProductAttribute("reniffects")]
 [assembly: System.Reflection.AssemblyTitleAttribute("reniffects")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
